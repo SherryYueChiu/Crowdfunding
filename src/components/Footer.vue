916bd98e73f970df195b47819d3cdfbfa1796372
Sherry <sherryyue.c@protonmail.com>
2024-01-10 02:35:55 +0800

    like coin 連結更新

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="copywriting">
-    <iframe data-v-b66e9a5a="" src="https://button.like.co/in/embed/sherryyue/button?referrer=https://dcard-avatar-hub-l9hj.vercel.app/" frameborder="0" class="lc-margin-top-64 lc-margin-bottom-32 lc-mobile"></iframe>
+    <iframe data-v-b66e9a5a="" src="https://button.like.co/in/embed/sherryyue/button?referrer=https://crowdfunding-sherryyue.vercel.app/" frameborder="0" class="lc-margin-top-64 lc-margin-bottom-32 lc-mobile"></iframe>
     <p>版權聲明：Copyright © 2023。保留所有權利。</p>
     <p>如有任何疑問或建議，可透過以下信箱聯繫：<a href="mailto:sherryyue.c@protonmail.com">sherryyue.c@protonmail.com</a>。</p>
   </div>
